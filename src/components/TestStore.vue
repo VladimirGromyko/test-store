4b1407c98388bd49fece394b00cb3685eef1a068
VladimirGromyko <vl-mailbox@mail.ru>
2023-11-27 14:49:08 +0300

    First commit

---
+++ src/components/TestStore.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<script>
+// <script setup>
+// import { ref } from 'vue'
+export default {
+  props: ["msg"],
+  data() {
+    return {
+      count: 0
+    }
+  },
+
+}
+// defineProps({
+//   msg: String,
+// })
+
+// const count = ref(0)
+
+</script>
+
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
