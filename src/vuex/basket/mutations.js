7aeada21cd27287f02e1582e6ae18cc6a6d08862
VladimirGromyko <vl-mailbox@mail.ru>
2023-11-29 17:54:18 +0300

    Add store. Add - make choice and delete item (didn't finish)-3.

--- src/vuex/basket/mutations.js
+++ src/vuex/basket/mutations.js
@@ -15,10 +15,13 @@ const mutations = {
         state.basket.forEach((el) => state.basket.totalCost += el.cost)
     },
     DELETE_FROM_BASKET(state, goods) {
+        debugger
         const productInd = state.basket.findIndex((el) => el.productId === goods.productId)
-        if (productInd >= 0 && state.basket[productInd].quantity > 0) {
+        if (productInd >= 0 && state.basket[productInd].quantity > 1) {
             state.basket[productInd].quantity--
             state.basket[productInd].cost = state.basket[productInd].quantity * state.basket[productInd].price
+        } else if (productInd >= 0 && state.basket[productInd].quantity === 1) {
+            state.basket = state.basket.filter((el) => el.productId !== state.basket[productInd].productId)
         } else {
             // state.basket.push(goods);
         }
