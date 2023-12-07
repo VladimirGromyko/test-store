e00e729bb5575884e19dc2e9ebe8a41a4e19f90f
VladimirGromyko <vl-mailbox@mail.ru>
2023-12-07 11:27:31 +0300

    The logic for calculating the total cost of goods in the cart has been simplified

--- src/vuex/basket/mutations.js
+++ src/vuex/basket/mutations.js
@@ -3,7 +3,6 @@ const mutations = {
     // Добавляем товар в корзину
     ADD_TO_BASKET(state, goods) {
         const purchases = state.basket.purchases
-        let totalCost = 0
         if (purchases.length === 0) {
             purchases.push(goods);
         } else {
@@ -15,14 +14,11 @@ const mutations = {
                 purchases.push(goods);
             }
         }
-        purchases.forEach((el) => totalCost += el.cost)
-        state.basket.totalCost = +totalCost.toFixed(1)
-
+        state.basket.totalCost = (+state.basket.totalCost + goods.cost).toFixed(1)
     },
 
     // Удаляем товар из корзины
     DELETE_FROM_BASKET(state, goods) {
-        let totalCost = 0
         let purchases = state.basket.purchases
         const productInd = purchases.findIndex((el) => el.productId === goods.productId)
         if (productInd >= 0 && purchases[productInd].quantity > 1) {
@@ -32,9 +28,7 @@ const mutations = {
         } else if (productInd >= 0 && purchases[productInd].quantity === 1) {
             state.basket.purchases = purchases.filter((el) => el.productId !== purchases[productInd].productId)
         }
-        state.basket.purchases.forEach((el) => totalCost += el.cost)
-        state.basket.totalCost = +totalCost.toFixed(1)
-
+        state.basket.totalCost = (+state.basket.totalCost - goods.cost).toFixed(1)
     },
 
     // Обновляем данные в корзине покупок, всвязи с изменением курса валюты
